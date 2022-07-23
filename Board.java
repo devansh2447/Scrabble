7ab052cfdd721aae170904ff2e321550138f2559
devansh2447 <devansh2447@gmail.com>
2022-07-23 12:47:04 +0530

    update double and triple words and letters

--- Board.java
+++ Board.java
@@ -15,16 +15,63 @@ public class Board
         int assign = 1;
         for(int row = 1; row < 16; row++){
             for(int column = 1; column < 16; column++){
-                this.squares[assign] = new Square(column, row, 1, 1);
-                //add code to check for double and triple words and letters - develop separate methods
-                //use AssignValues class
+                this.squares[assign] = new Square(column, row, getLetterMultiply(column, row), getWordMultiply(column, row));
                 assign++;
             }
         }
     }
     
-    public void getDoubleLetter(){
-        
+    public static int getLetterMultiply(int column, int row){
+        String doubleSquares = getDoubleLetter();
+        String tripleSquares = getTripleLetter();
+        String check = column + "," + row;
+        if(doubleSquares.contains(check)){
+            return 2;
+        }
+        if(tripleSquares.contains(check)){
+            return 3;
+        }
+        return 1;
+    }
+
+    public static int getWordMultiply(int column, int row){
+        String tripleSquares = getTripleWord();
+        String check = column + "," + row;
+        if(getDoubleWord(column, row)){
+            return 2;
+        }
+        if(tripleSquares.contains(check)){
+            return 3;
+        }
+        return 1;
+    }
+
+    public static String getDoubleLetter(){
+        return "4,1,,12,1,,7,3,,9,3,,1,4,,8,4,,15,4,,3,7,,7,7,,9,7,,13,7,,4,8,,12,8,,3,9,,7,9,,9,9,,13,9,,1,12,,8,12,,15,12,,7,13,,9,13,,4,15,,12,15";
+    }
+
+    public static String getTripleLetter(){
+        return "6,2,,10,2,,2,6,,6,6,,10,6,,14,6,,2,10,,6,10,,10,10,,14,10,,6,14,,10,14";
+    }
+
+    public static boolean getDoubleWord(int column, int row){
+        if(column == row || 15 - column + 1 == row){
+            if(column == 8){
+                return true;
+            }
+            if(column > 1 && column < 6){
+                return true;
+            }
+            else if(column > 11 && column < 15){
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+
+    public static String getTripleWord(){
+        return "1,1,,8,1,,15,1,,1,8,,15,8,,1,15,,8,15,,15,15";
     }
 
     public boolean add(int x, int y, String name){
